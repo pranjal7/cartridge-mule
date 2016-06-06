09724e6c48094df9a04b4bdb11fe0431eb6b5580
nikolaos.karapanos <nikolaos.karapanos@accenture.com>
2016-06-06 16:17:59 +0100

    fixed gut url

--- jenkins/jobs/dsl/create_mule_environment.groovy
+++ jenkins/jobs/dsl/create_mule_environment.groovy
@@ -15,7 +15,7 @@ createMuleStack.with{
 		numToKeep(25)
     }
 	parameters{
-		stringParam("GIT_URL","git@newsource.accenture.com:a2482/mule_environment_template","The URL of the git repo for Platform Extension")
+		stringParam("GIT_URL","ssh://git@newsource.accenture.com/a2482/mule_environment_template.git","The URL of the git repo for Platform Extension")
 		stringParam("STACK_NAME","","The name of the new stack")
 		stringParam("TAG_PROJECT_NAME","","The name of the project to tag instances with")
         stringParam("KEY_NAME","academy_key","Name of the key for this stack")
