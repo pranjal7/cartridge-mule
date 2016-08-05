6ea506084ef3c5131a55d78fe582a9068be2d4a6
nikolaos.karapanos <nikolaos.karapanos@accenture.com>
2016-08-05 13:10:18 +0100

    fixed a code error in mule deploy jenkins job

--- jenkins/jobs/dsl/mule_jobs.groovy
+++ jenkins/jobs/dsl/mule_jobs.groovy
@@ -380,14 +380,14 @@ do
 done
 
 ssh -tt -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ec2-user@${MULE_EE_SERVER_IP} '
-      sudo swarm stop mule-runtime
+      sudo docker --config=/home/ec2-user/docker_auth_custom_registry --tlsverify --tlscacert=/home/ec2-user/.certs/ca.pem --tlscert=/home/ec2-user/.certs/cert.pem --tlskey=/home/ec2-user/.certs/key.pem -H manager.adop.internal:3376 stop mule-runtime
       sudo rm -rf /data/mule/apps/*
       for i in $(find ~/. -name *.zip)
       do
         sudo unzip ${i} -d /data/mule/apps/$(basename ${i} .zip) 1>/dev/null
       done
       
-      sudo swarm start mule-runtime
+      sudo docker --config=/home/ec2-user/docker_auth_custom_registry --tlsverify --tlscacert=/home/ec2-user/.certs/ca.pem --tlscert=/home/ec2-user/.certs/cert.pem --tlskey=/home/ec2-user/.certs/key.pem -H manager.adop.internal:3376 start mule-runtime
 '
 
 echo 
