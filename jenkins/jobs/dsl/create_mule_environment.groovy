7cbd3ecf69eac0ba40dcbffa6ff4b0ca8d99838c
Bhatnagar <pranjal.bhatnagar@accenture.com>
2017-05-31 09:39:25 +0100

    added my github instead of aws s3 bucket

--- jenkins/jobs/dsl/create_mule_environment.groovy
+++ jenkins/jobs/dsl/create_mule_environment.groovy
@@ -22,7 +22,6 @@ createMuleStack.with{
 		stringParam("PRIVATE_IP","10.0.6.6","PrivateIp address for Mule Env")
 		choiceParam("MuleSoft_License",['Accenture_Demo (default)', 'External'], "License use")
 		fileParam('$HOME', 'my description')
-		//choiceParam("MuleSoft_License_choice",if (MuleSoft_License.equals("Accenture_Demo") { return["Do nothing"]} else if (MuleSoft_License.equals("External") {return["Change license below"]}))  "Accenture_Demo" "External", "License use")
 		stringParam("AWS_DEFAULT_REGION","eu-west-1","AWS Default Region")
 		credentialsParam("AWS_CREDENTIALS"){
 			type('com.cloudbees.plugins.credentials.impl.UsernamePasswordCredentialsImpl')
